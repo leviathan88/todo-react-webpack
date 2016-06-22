f67b21cbad91b72d617245b825e19347cf6834f4
leviathan88 <levigithuber@gmail.com>
2016-06-22 18:29:19 +0200

    creating edit and cancel buttons and their methods

--- src/components/todos-list-item.js
+++ src/components/todos-list-item.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react'
 
 class TodosListItem extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      isEditing: false
+    }
+  }
+
+  editItem() {
+    this.setState({
+        isEditing: true
+    })
+  }
+
+  cancelEditItem() {
+    this.setState({
+      isEditing: false
+    })
+  }
+
+  renderActionsSection() {
+    const { isEditing } = this.state
+
+    if(isEditing) return (
+      <td>
+        <button>Save</button>
+        <button onClick={this.cancelEditItem.bind(this)}>Cancel</button>
+      </td>
+    )
+
+    return (
+      <td>
+        <button onClick={this.editItem.bind(this)}>Edit</button>
+        <button>Delete</button>
+      </td>
+    )
+  }
+
   render() {
     return (
       <tr>
         <td>{ this.props.task }</td>
-        <td>
-          <button>Edit</button>
-          <button>Delete</button>
-        </td>
+        {this.renderActionsSection()}
       </tr>
     )
   }
