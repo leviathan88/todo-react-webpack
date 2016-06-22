be891d244f7a5ce87670548bcae55dfebe128ade
leviathan88 <levigithuber@gmail.com>
2016-06-22 19:10:17 +0200

    adding loaders for bootstrap and scss

--- webpack.config.js
+++ webpack.config.js
@@ -18,11 +18,35 @@ module.exports = {
     },
     module: {
         loaders: [
-        {
+          {
             test: /\.jsx?$/,
             exclude: /node_modules/,
             loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
-        }
+          },
+          {
+            test: /\.css/,
+            loaders: ['style', 'css']
+          },
+          {
+            test: /\.scss/,
+            loaders: ['style', 'css', 'sass']
+          },
+          {
+            test: /\.(mp4|ogg|svg)$/,
+            loader: 'file-loader'
+          },
+          {
+            test: /\.(png|jpg|gif|woff|woff2)$/,
+            loader: 'url-loader?limit=8192'
+          },
+          {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url?limit=10000&mimetype=application/octet-stream"
+          },
+          {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "file"
+          }
         ]
     },
     plugins: [
