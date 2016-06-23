b459a9dd8ff91f119e80578b48bdab6063824237
leviathan88 <levigithuber@gmail.com>
2016-06-23 16:30:03 +0200

    creating validation input

--- src/components/todos-create.js
+++ src/components/todos-create.js
@@ -2,11 +2,45 @@ import React, { Component } from 'react'
 
 class TodosCreate extends Component {
 
+  constructor() {
+    super()
+
+    this.state = {
+      error: null
+    }
+  }
+
   handleCreate(event) {
     event.preventDefault()
+
+    const task = this.refs.createInput.value
     const { createTask } = this.props
-    createTask(this.refs.createInput.value)
+    const validateInput = this.validateInput(task)
+
+    if (validateInput) {
+      this.setState({
+        error: validateInput
+      })
+      return
+    }
+
+    this.setState({error: null})
+    createTask(task)
     this.refs.createInput.value = ''
+  }
+
+  validateInput(task) {
+    const { alreadyExists } = this.props
+    if(!task) return "Please enter a task!"
+    if (alreadyExists(task)) return "Task already exists!"
+    return null;
+  }
+
+  renderError() {
+    const { error } = this.state
+    if(error) {
+      return <div  className="alert alert-warning" ><strong>{ error }</strong></div>
+    }
   }
 
   render() {
@@ -14,6 +48,7 @@ class TodosCreate extends Component {
       <form onSubmit={this.handleCreate.bind(this)}>
         <input type="text" placeholder="what do i need to do" ref="createInput"/>
         <button className="btn btn-primary">Create</button>
+        {this.renderError()}
       </form>
     )
   }
